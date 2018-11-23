f6bfe928a74050189fa8c88687c50e6f48329470
luthesebas <luthesebas@gmail.com>
2018-11-23 15:36:03 +0100

    Implemented EventService

---
+++ call-app/call-app/Services/EventService.cs
@@ -0,0 +1,23 @@
+﻿using call_app.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace call_app.Services
+{
+    public class EventService
+    {
+        public Event Create(CreateEvent createEvent)
+        {
+            var request = new RestRequest
+            {
+                Resource = "create-event",
+                Method = Method.POST
+            };
+            request.AddBody(createEvent);
+            return Rest.Send<Event>(request);
+        }
+
+    }
+}
