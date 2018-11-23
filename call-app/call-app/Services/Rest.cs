e097e0fa32785377a15780fb6cc5edb1d82b7bfb
luthesebas <luthesebas@gmail.com>
2018-11-23 15:00:56 +0100

    Rest class is package private now

--- call-app/call-app/Services/Rest.cs
+++ call-app/call-app/Services/Rest.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace call_app.Services
 {
-    public class Rest
+    class Rest
     {
         private const string BaseUrl = "https://nr59y0tklh.execute-api.eu-west-1.amazonaws.com/dev";
 
@@ -13,12 +13,10 @@ namespace call_app.Services
             {
                 BaseUrl = new Uri(BaseUrl)
             };
-            Console.WriteLine("### Request: " + request);
             var response = client.Execute<T>(request);
 
             if (response.IsSuccessful)
             {
-                Console.WriteLine("### Response: " + response.Data);
                 return response.Data;
             }
             else
