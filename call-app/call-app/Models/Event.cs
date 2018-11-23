63ff03444749dbfb92b92fdce5a25ba70eef996d
luthesebas <luthesebas@gmail.com>
2018-11-23 15:35:44 +0100

    Added Event model

---
+++ call-app/call-app/Models/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace call_app.Models
+{
+    public class Event
+    {
+        public string UserId { get; set; }
+        public string EventId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Event() { }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "User(userid={0}, eventid={1}, title={2}, description={3})", 
+                UserId, EventId, Title, Description);
+        }
+    }
+}
