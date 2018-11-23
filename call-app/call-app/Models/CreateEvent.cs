5fb7d3a7a36e82a5b7cf22491f61a69fc410ada5
luthesebas <luthesebas@gmail.com>
2018-11-23 15:35:54 +0100

    Added CreateEvent model

---
+++ call-app/call-app/Models/CreateEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace call_app.Models
+{
+    public class CreateEvent
+    {
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public CreateEvent(string userId, string title, string description = null)
+        {
+            UserId = userId;
+            Title = title;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "User(userid={0}, title={1}, description={2})", 
+                UserId, Title, Description);
+        }
+    }
+}
