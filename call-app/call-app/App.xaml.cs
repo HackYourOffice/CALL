275a484a2077f2469212282e139f5e7148f8bfea
Seledtsova, Daria <Daria.Seledtsova@dm.de>
2018-11-24 04:43:16 +0100

    add polling

--- call-app/call-app/App.xaml.cs
+++ call-app/call-app/App.xaml.cs
@@ -5,6 +5,8 @@ using call_app.Views;
 using call_app.Services;
 using call_app.Models;
 using call_app.Services.Users;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace call_app
@@ -17,22 +19,45 @@ namespace call_app
             MainPage = new MainPage();
         }
 
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-            var user = new User();
-            if (Application.Current.Properties.ContainsKey("userId"))
-            {
-                user.UserId = Application.Current.Properties["userId"] as string;
-            }
-            else
-            {
-                var userService = new UserService();
-                user = userService.Create();
-                Application.Current.Properties.Add("userId", user.UserId);
-            }
-            Share.User = user;
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+			var user = new User();
+			if (Application.Current.Properties.ContainsKey("userId"))
+			{
+				user.UserId = Application.Current.Properties["userId"] as string;
+			}
+			else
+			{
+				var userService = new UserService();
+				user = userService.Create();
+				Application.Current.Properties.Add("userId", user.UserId);
+			}
+			Share.User = user;
+
+            StartPolling();
+		}
+
+		private void StartPolling() 
+		{
+			EventService eventService = new EventService();
             
+			System.Threading.Tasks.Task.Run(async () =>
+			{
+			while (true)
+			{
+				List<Event> events = eventService.Poll();
+					if (events != null && events.Count != 0)
+					{
+						Console.WriteLine("#####" + events[0].Title);
+					} 
+					else 
+					{
+						Console.WriteLine("##### Ohh, no events registered");
+					}
+					await System.Threading.Tasks.Task.Delay(3000);
+				}
+			}).ConfigureAwait(false);
 		}
 
         protected override void OnSleep()
