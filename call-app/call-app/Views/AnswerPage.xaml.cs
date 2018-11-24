c08ebfffa872ed7db150a5da658314fc2c9766b7
Louicha <22676238+Louicha@users.noreply.github.com>
2018-11-24 05:28:18 +0100

    Edit AnswerPage

--- call-app/call-app/Views/AnswerPage.xaml.cs
+++ call-app/call-app/Views/AnswerPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using call_app.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,12 @@ namespace call_app.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AnswerPage : ContentPage
 	{
+        Event currentEvent = new Event
+        {
+            Title =  "DFIRGJIEG"
+        }; 
+
+
 		public AnswerPage ()
 		{
             InitializeComponent();
@@ -19,11 +26,13 @@ namespace call_app.Views
        
         async void On5MinClicked(object sender, EventArgs args)
         {
-            //await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+            int time = 5;
+            await Navigation.PushModalAsync(new NavigationPage(new CountdownPage(time)));
         }
         async void On10MinClicked(object sender, EventArgs args)
         {
-          //  await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+            int time = 10;
+            await Navigation.PushModalAsync(new NavigationPage(new CountdownPage(time)));
         }
 
 	}
---
+++ call-app/call-app/Views/CountdownPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace call_app.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CountdownPage : ContentPage
+	{
+		public CountdownPage (int time)
+		{
+			InitializeComponent ();
+		}
+	}
+}
