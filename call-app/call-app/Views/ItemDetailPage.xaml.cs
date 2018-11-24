b1ee489621f151a62a948fe47c8d4b6136dddab5
Seledtsova, Daria <Daria.Seledtsova@dm.de>
2018-11-24 03:50:50 +0100

    registriere mich button

--- call-app/call-app/Views/ItemDetailPage.xaml.cs
+++ call-app/call-app/Views/ItemDetailPage.xaml.cs
@@ -5,19 +5,23 @@ using Xamarin.Forms.Xaml;
 
 using call_app.Models;
 using call_app.ViewModels;
+using call_app.Services;
 
 namespace call_app.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ItemDetailPage : ContentPage
     {
-        ItemDetailViewModel viewModel;
+		ItemDetailViewModel ViewModel;
+		EventService eventService;
+		Boolean registered = false;
 
         public ItemDetailPage(ItemDetailViewModel viewModel)
         {
             InitializeComponent();
+			eventService = new EventService();
 
-            BindingContext = this.viewModel = viewModel;
+            BindingContext = this.ViewModel = viewModel;
         }
 
         public ItemDetailPage()
@@ -30,8 +34,24 @@ namespace call_app.Views
                 Description = "This is an event description."
             };
 
-			viewModel = new ItemDetailViewModel(myEvent);
-            BindingContext = viewModel;
+			ViewModel = new ItemDetailViewModel(myEvent);
+            BindingContext = ViewModel;
         }
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			if (!registered) {
+				if (eventService.RegisterAt(ViewModel.Event))
+                {
+                    (sender as Button).Text = "Event verlassen";
+					registered = !registered;
+                }				
+			} else {
+				(sender as Button).Text = "Registriere mich!";
+				registered = !registered;
+			}
+
+
+		}
     }
 }
