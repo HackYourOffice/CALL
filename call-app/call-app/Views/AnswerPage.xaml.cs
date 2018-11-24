a366ad60c2e37c4b86997fefe5cf3bca4b72690e
Louicha <22676238+Louicha@users.noreply.github.com>
2018-11-24 04:52:17 +0100

    Add Buttons

---
+++ call-app/call-app/Views/AnswerPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace call_app.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AnswerPage : ContentPage
+	{
+		public AnswerPage ()
+		{
+            InitializeComponent();
+        }
+       
+        async void On5MinClicked(object sender, EventArgs args)
+        {
+            //await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        }
+        async void On10MinClicked(object sender, EventArgs args)
+        {
+          //  await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        }
+
+	}
+}
